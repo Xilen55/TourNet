ddfb9154cb3877b42f58ef429bab4ee3a7b27ddc
Xilen55 <133131396+Xilen55@users.noreply.github.com>
2025-06-30 14:11:49 -0300

    tournet.sql

--- TourNet/tournet.sql
+++ TourNet/tournet.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-06-2025 a las 05:50:07
+-- Tiempo de generación: 30-06-2025 a las 19:09:52
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -35,14 +35,6 @@ CREATE TABLE `carrito` (
   `fecha_agregado` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Volcado de datos para la tabla `carrito`
---
-
-INSERT INTO `carrito` (`id_carrito`, `id_usuario`, `id_producto`, `cantidad`, `fecha_agregado`) VALUES
-(6, 4, 1, 4, '2025-06-24 18:11:01'),
-(7, 4, 1, 1, '2025-06-24 18:11:06');
-
 -- --------------------------------------------------------
 
 --
@@ -63,7 +55,10 @@ CREATE TABLE `compras` (
 
 INSERT INTO `compras` (`id_compra`, `id_usuario`, `fecha_compra`, `total`, `estado`) VALUES
 (2, 4, '2025-06-19 16:38:08', 2000.00, 'confirmado'),
-(3, 4, '2025-06-23 03:54:33', 10000.00, 'anulado');
+(3, 4, '2025-06-23 03:54:33', 10000.00, 'anulado'),
+(5, 4, '2025-06-25 14:06:49', 90000.00, 'pendiente'),
+(6, 5, '2025-06-25 14:21:27', 8000.00, 'pendiente'),
+(7, 4, '2025-06-25 15:03:12', 4500.00, 'pendiente');
 
 -- --------------------------------------------------------
 
@@ -86,7 +81,15 @@ CREATE TABLE `detalle_compras` (
 INSERT INTO `detalle_compras` (`id_detalle`, `id_compra`, `id_producto`, `cantidad`, `precio_unitario`) VALUES
 (2, 2, 1, 1, 2000.00),
 (3, 3, 1, 3, 2000.00),
-(4, 3, 1, 2, 2000.00);
+(4, 3, 1, 2, 2000.00),
+(6, 5, 1, 4, 1000.00),
+(7, 5, 1, 1, 1000.00),
+(8, 5, 1, 1, 1000.00),
+(9, 5, 2, 56, 1500.00),
+(10, 6, 1, 2, 1000.00),
+(11, 6, 2, 4, 1500.00),
+(12, 7, 1, 3, 1000.00),
+(13, 7, 2, 1, 1500.00);
 
 -- --------------------------------------------------------
 
@@ -112,7 +115,8 @@ CREATE TABLE `productos` (
 --
 
 INSERT INTO `productos` (`id_producto`, `tipo`, `nombre`, `descripcion`, `destino`, `precio`, `duracion_dias`, `fecha_disponible`, `stock_disponible`, `imagen_url`) VALUES
-(1, 'pasaje', 'Miami - Pasaje', 'Pasaje de vuelo a Miami', 'Miami', 2000.00, 1, '0000-00-00', 10, 'https://www.infobae.com/resizer/v2/4UDUKPQSIBH65GLU3M7GV6XMC4.jpg?auth=a323616c5df428ff4b9a1b852a81e0dc043e2de7ead00f402bb31f326635c03a&smart=true&width=1200&height=800&quality=85');
+(1, 'pasaje', 'Buenos Aires -> Miami Pasaje', 'Pasaje de vuelo desde Buenos Aires a Miami', 'Miami', 1000.00, 1, '2025-12-10', 10, 'https://resizer.glanacion.com/resizer/v2/miami-beach-tiene-a-un-lado-el-oceano-atlantico-SJF6LSF5NZE3LKQFN6PJ6LVNYY.jpg?auth=af70bf07462a10a2bc6fc4756b59af449b88ea56de37160417d1b625009e3d69&width=420&height=281&quality=70&smart=true'),
+(2, 'pasaje', 'Buenos Aires -> Barcelona\r\nPasaje', 'Pasaje de vuelo desde Buenos Aires a Barcelona', 'Barcelona', 1500.00, 1, '2025-12-10', 10, 'https://a1.elespanol.com/metropoliabierta/2019/06/27/vivir-en-barcelona/vivir_en_barcelona_409469054_9937911_1024x576.jpg');
 
 -- --------------------------------------------------------
 
@@ -133,8 +137,9 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id_usuario`, `nombre`, `email`, `contraseña`, `rol`) VALUES
-(1, 'Juan Giaveno', 'juangiaveno07@gmail.com', '$2y$10$JMFY.f6BibvK.vuhLGy.Y.kxBYXC6r3h3Wx2rVbKXsFf6lfxi3PJi', 'admin'),
-(4, 'Pepo De La Vega', 'pepodelavega@gmail.com', '$2y$10$oYZVwSTTMN10TOJKrtDIYuj70dUSneH/NAoEpzmYACEIQM2tLMMMC', 'cliente');
+(1, 'Administrador', 'tournet@gmail.com', '$2y$10$JMFY.f6BibvK.vuhLGy.Y.kxBYXC6r3h3Wx2rVbKXsFf6lfxi3PJi', 'admin'),
+(4, 'Pepo De La Vega', 'pepodelavega@gmail.com', '$2y$10$oYZVwSTTMN10TOJKrtDIYuj70dUSneH/NAoEpzmYACEIQM2tLMMMC', 'cliente'),
+(5, 'Morgan Rogers', 'morganrogers27@escuelasproa.edu.ar', '$2y$10$R29bP2UvIhPbfqW/NXsQY.rosCpxQwP1UT30.AOCepM3Z.r21xnvy', 'cliente');
 
 --
 -- Índices para tablas volcadas
@@ -184,31 +189,31 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `carrito`
 --
 ALTER TABLE `carrito`
-  MODIFY `id_carrito` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_carrito` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `compras`
 --
 ALTER TABLE `compras`
-  MODIFY `id_compra` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_compra` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `detalle_compras`
 --
 ALTER TABLE `detalle_compras`
-  MODIFY `id_detalle` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_detalle` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Restricciones para tablas volcadas
